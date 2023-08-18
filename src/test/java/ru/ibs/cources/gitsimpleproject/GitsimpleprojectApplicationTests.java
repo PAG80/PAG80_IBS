72ceda02668b78a313b6e9c42b30be171ded1358
PAG80 <135762157+PAG80@users.noreply.github.com>
2023-08-18 23:03:17 +0300

    init simple project

---
+++ src/test/java/ru/ibs/cources/gitsimpleproject/GitsimpleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ibs.cources.gitsimpleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsimpleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
